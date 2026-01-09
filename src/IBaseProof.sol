6cb90235bae8dfa308a092221422f026e27d5f33
dimka90 <dimkayilrit@gmail.com>
2026-01-09 22:30:59 +0100

    feat: update interface signatures for metadata

--- src/IBaseProof.sol
+++ src/IBaseProof.sol
@@ -8,13 +8,31 @@ interface IBaseProof {
     error InvalidSignature();
     error DeadlineExpired();
     error Unauthorized();
-    
+
     event ProofSubmitted(address indexed user, bytes32 indexed proofHash, uint256 timestamp);
     event BatchProofSubmitted(address indexed user, uint256 count, uint256 timestamp);
     event Paused(address account);
     event Unpaused(address account);
     event VerifierAdded(address indexed verifier);
     event VerifierRemoved(address indexed verifier);
-    
-    function submitProof(bytes32 proofHash, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external;
+
+    function submitProof(
+        bytes32 proofHash, 
+        bytes32 metadataHash, 
+        uint256 deadline, 
+        uint8 v, 
+        bytes32 r, 
+        bytes32 s
+    ) external;
+
+    function submitProofBatch(
+        bytes32[] calldata proofHashes,
+        bytes32[] calldata metadataHashes,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+
+    function isProofSubmitted(bytes32 proofHash) external view returns (bool);
 }
