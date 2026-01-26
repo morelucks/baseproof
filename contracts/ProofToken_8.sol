cb84fd9c6e2a87afa569530e4736862b8f29c17c
Kefas Kingsley <kefasiceking@gmail.com>
2026-01-26 19:24:11 +0100

    Add ProofToken_8 smart contract

---
+++ contracts/ProofToken_8.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ProofToken8 {
+    string public name = "ProofToken 8";
+    uint256 public timestamp = block.timestamp;
+    constructor() {}
+}
