19fefe1ae954fb529f40966881c7da8d9559e55f
Kefas Kingsley <kefasiceking@gmail.com>
2026-01-26 19:24:11 +0100

    Add ProofToken_12 smart contract

---
+++ contracts/ProofToken_12.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ProofToken12 {
+    string public name = "ProofToken 12";
+    uint256 public timestamp = block.timestamp;
+    constructor() {}
+}
